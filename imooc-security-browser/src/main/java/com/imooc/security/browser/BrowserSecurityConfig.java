307422275b70e7565cdad9ea0e405a6970f206a7
rqxiao <1007689622@qq.com>
2020-01-19 14:52:00 +0800

    4-4个性化用户认证流程（一）自定义登录页面

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -30,10 +30,15 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin()//表单登录进行身份认证
 //        http.httpBasic()//httpBasic登录进行身份认证
+                .loginPage("/imooc-signIn.html")//配置登录页面（但是这样写无论是浏览器还是接口调用都只会返回到页面上）
+                .loginProcessingUrl("/authentication/form")//登录访问的url 即UsernamePasswordAuthenticationFilter会来处理这个请求
                 .and()
                 .authorizeRequests()//对下面请求
+                .antMatchers("/imooc-signIn.html").permitAll()//对这个请求不需要身份验证
                 .anyRequest()//任何请求
-                .authenticated();//已经授权
+                .authenticated()//都需要认证/登录
+                .and()
+                .csrf().disable();
 
         super.configure(http);
     }
